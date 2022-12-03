6e4747e8d0ea901b1763948087dd722bbf522569
sooyoungkang <kang.sooyoung@hotmail.com>
2022-12-03 23:21:25 +0900

    Update Encryption Method

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -8,11 +8,6 @@ import { APIKeyUtils } from "../configs/security/utils/APIKeyUtils";
 export class BaseController {
   private apiKeyUtils: APIKeyUtils = new APIKeyUtils();
 
-  protected checkAuth = (apiKey: string, serviceId: string) =>
-    // this.apiKeyUtils.validateKey(apiKey, serviceId);
-    {
-      const res = this.apiKeyUtils.createKeyForServiceId("0418");
-      console.log(res);
-      return true;
-    };
+  protected checkAuth = (apiKey: string) =>
+    this.apiKeyUtils.validateKey(apiKey);
 }
